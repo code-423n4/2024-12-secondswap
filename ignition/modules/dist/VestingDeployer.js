12e9a3693cfbdf0a7115080d0fca027501bf1768
Darel Low <darel@techtics.io>
2024-12-09 15:00:49 +0800

    [Fixed] npx hardhat test failed

--- ignition/modules/dist/VestingDeployer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var modules_1 = require("@nomicfoundation/hardhat-ignition/modules");
-exports["default"] = modules_1.buildModule("VestingDeployer", function (m) {
-    var deployer = m.contract("SecondSwap_VestingDeployer");
-    return { deployer: deployer };
-});
